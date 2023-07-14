01d8a3374d7ad09b0ebf572f1b22e442f4817ed1
shrikap <shrikapunuganti@gmail.com>
2023-07-14 09:25:41 -0700

    Initial Commit

---
+++ navigationstack/navigationstackApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationstackApp.swift
+//  navigationstack
+//
+//  Created by Scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct navigationstackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
